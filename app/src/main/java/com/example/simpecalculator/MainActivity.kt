d6b217e6d8ab711e12b15623201e6b5fb1423324
Papazsende <bariscetintr@gmail.com>
2022-06-27 21:04:14 +0300

    added append function with clear option

--- app/src/main/java/com/example/simpecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simpecalculator/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.simpecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInput = findViewById(R.id.tvInput)
     }
+
+    public fun append(view:View){
+
+        when((view as Button).text){
+            "CLR" -> tvInput?.text = ""
+            else -> tvInput?.append((view as Button).text)
+        }
+    }
+
+
+
 }
