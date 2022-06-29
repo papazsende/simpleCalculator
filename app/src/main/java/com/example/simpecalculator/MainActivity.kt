810ac77cf6df24bafb6d28d4ac1be29b65d5bf6e
Papazsende <bariscetintr@gmail.com>
2022-06-29 19:34:03 +0300

    Added decimal and operators

--- app/src/main/java/com/example/simpecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simpecalculator/MainActivity.kt
@@ -9,22 +9,61 @@ import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
-    private var tvInput : TextView? = null
-
+    private var tvInput: TextView? = null
+    var lastNumeric: Boolean = false
+    var lastDecimal: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvInput = findViewById(R.id.tvInput)
     }
 
-    public fun append(view:View){
+    public fun append(view: View) {
 
-        when((view as Button).text){
+        when ((view as Button).text) {
             "CLR" -> tvInput?.text = ""
-            else -> tvInput?.append((view as Button).text)
+            "." -> {
+                if (lastNumeric && !lastDecimal) {
+                    tvInput?.append((view as Button).text)
+                    lastDecimal = true
+                    lastNumeric = false
+                } else {
+
+                }
+
+            }
+            "+", "-", "/", "*" -> onOperator(view)
+            else -> {
+                tvInput?.append((view as Button).text)
+                lastDecimal = false
+                lastNumeric = true
+            }
         }
     }
 
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("+") ||
+                    value.contains("/") ||
+                    value.contains("*") ||
+                    value.contains("-")
+        }
+    }
+
+    private fun onOperator(view: View) {
+        tvInput?.text?.let {
+
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDecimal = false
+            }
+
+
+        }
+    }
 
 
 }
